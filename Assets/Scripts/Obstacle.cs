3c62e887f934a5e03d195215ff7f8728c9d54d7f
Juwon Cha <littlebigcha@gmail.com>
2025-07-23 09:19:53 +0900

    [INIT] Initialize MyUnityWorld

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // 장애물이 상하로 이동할 때 얼마나 이동할지에 대한 수치
+    public float HighPosY = 1f;
+    public float LowPosY = -1f;
+
+    // 탑과 바텀 사이의 공간을 얼마나 가져갈지에 대한 수치
+    public float HoleSizeMin = 1f;
+    public float HoleSizeMax = 3f;
+
+    public Transform TopObject;
+    public Transform BottomObject;
+
+    public float WidthPadding = 4f;
+
+    private GameManager mGameManager;
+
+    private void Start()
+    {
+        mGameManager = GameManager.Instance;
+    }
+
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
+    {
+        float holeSize = Random.Range(HoleSizeMin, HoleSizeMax);
+        float halfHoleSize = holeSize / 2;
+
+        // 자식 오브젝트(Top, Bottom) localPosition을 변경하면 부모의 위치를 기준으로 움직임
+        TopObject.localPosition = new Vector3(0, halfHoleSize, 0); // HoleSize 반만큼 위로 올림
+        BottomObject.localPosition = new Vector3(0, -halfHoleSize, 0); // HoleSize 반만큼 아래로 내림
+
+        // 부모 오브젝트(Obstacle)
+        // 제일 마지막에 놓여진 장애물 뒤에다가 WidthPadding 만큼 더해서 배치
+        Vector3 placePosition = lastPosition + new Vector3(WidthPadding, 0, 0);
+        placePosition.y = Random.Range(LowPosY, HighPosY); // 장애물 높이는 랜덤
+
+        transform.position = placePosition;
+
+        return placePosition;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if(player)
+        {
+            mGameManager.AddScore(1);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI RestartText;
+
+    void Start()
+    {
+        if(RestartText == null)
+        {
+            Debug.LogError("Restart Text is null");
+        }
+
+        if(ScoreText == null)
+        {
+            Debug.LogError("Score Text is null");
+        }
+
+        RestartText.gameObject.SetActive(false);
+    }
+
+    public void SetRestart()
+    {
+        RestartText.gameObject.SetActive(true);
+    }
+
+    public void UpdateScore(int score)
+    {
+        ScoreText.text = score.ToString();
+    }
+}
