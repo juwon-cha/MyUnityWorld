3cd40972a51a87a4f9b15ca7b1b765dce1e3a633
Juwon Cha <littlebigcha@gmail.com>
2025-07-24 23:58:19 +0900

    [FEAT] 상호작용 팝업 추가

--- Assets/Scripts/MyUnityWorld/EventTrigger/TriggerDetection.cs
+++ Assets/Scripts/MyUnityWorld/EventTrigger/TriggerDetection.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TriggerDetection : MonoBehaviour
 {
+    enum TriggerType
+    {
+        MINI_GAME,
+        NPC,
+        LEADER_BOARD,
+        MIRROR
+    }
+
+    [SerializeField] private GameObject _interaction;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("Player entered the trigger area: " + gameObject.name);
+            _interaction.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _interaction.SetActive(false);
         }
     }
 }
